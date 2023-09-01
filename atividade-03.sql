8f283b538aa7f60cce3796753b3d32a4ad61281b
Ryan Alves <ra546038@gmail.com>
2023-09-01 17:26:07 -0300

    atividade_03

--- atividade-03.sql
+++ atividade-03.sql
@@ -82,11 +82,11 @@ insert into estoque (id, carro_id, quantidade_estoque) values
 
 -- 1.  Escreva uma consulta para listar todos os carros disponíveis na loja.
 
-	select * from carros; 
+	select * from carros where quantidade_estoque > 0; 
 
 -- 2.  Recupere os detalhes de um carro específico usando seu ID.
 
-	select * from carros where id=1;
+	select * from carros where id = 1;
 
 -- 3.  Mostre o nome, o CPF e o e-mail de todos os clientes cadastrados.
 
@@ -94,39 +94,66 @@ insert into estoque (id, carro_id, quantidade_estoque) values
 
 -- 4.  Recupere os detalhes de uma compra específica, incluindo o nome do cliente, a data da compra e o total.
 
-	select nome, data_compra, total_compra from clientes inner join compras on clientes.id = compras.cliente_id where compras.id = 2; 
+	select clientes.nome, compras.data_compra, compras.total_compra from clientes inner join compras on clientes.id = compras.cliente_id where compras.id = 2; 
 
 -- 5.  Liste os carros que estão em estoque, juntamente com a quantidade disponível.
 
-	select nome, quantidade_estoque from carros where quantidade_estoque > 0;
+	select nome, quantidade_estoque from carros 
+    where quantidade_estoque > 0;
 
 -- 6.  Crie uma consulta para mostrar os clientes que compraram um determinado carro (usando o ID do carro).
 
-	select clientes.nome from clientes inner join compras on clientes.id = compras.cliente_id inner join carros_compra on compras.id = carros_compra.compra_id where carros_compra.carro_id > 0 ;
+	select clientes.nome from 
+    clientes inner join compras on clientes.id = compras.cliente_id 
+    inner join carros_compra on compras.id = carros_compra.compra_id 
+    where carros_compra.carro_id = 1 ;
     
-    -- carros -> carros_compra -> compras -> cliente
-
+-- 6.2  Crie uma consulta para mostrar os clientes e o nome do carro que compraram um determinado carro (usando o ID do carro).
+    
+    select clientes.nome, carros.nome from 
+    clientes inner join compras on clientes.id = compras.cliente_id 
+    inner join carros_compra on compras.id = carros_compra.compra_id 
+    inner join carros on carros_compra.carro_id = carros.id
+    where carros_compra.carro_id = 1 ;
+    
 -- 7.  Exiba o total de cada compra, juntamente com os nomes dos carros comprados, para um cliente específico.
 
-	-- select total_compra, carro.nome;
+	select compras.total_compra, carros.nome from 
+    clientes inner join compras on clientes.id = compras.cliente_id 
+    inner join carros_compra on compras.id = carros_compra.compra_id
+    inner join carros on carros_compra.carro_id = carros.id
+    where clientes.id = 1;
 
 -- 8.  Mostre os carros que foram comprados em uma determinada data.
 
-select nome from carros inner join compras on carros.id = compras.id where data_compra = "1989-12-13";
+	select carros.nome from 
+    carros inner join compras on carros.id = compras.id 
+    where data_compra = "1989-12-13";
 
 -- 9.  Recupere o total de vendas de cada cliente (nome e total) em ordem decrescente.
 
+	select clientes.nome, compras.total_compra from
+    clientes join compras on clientes.id = compras.cliente_id 
+    order by compras.total_compra desc;
+
 -- 10.  Exiba o nome do cliente que fez a compra mais cara até o momento.
 
+	select clientes.nome, compras.total_compra 
+    from clientes 
+    join compras on clientes.id = compras.cliente_id 
+    order by compras.total_compra desc
+    limit 1;
+    
 -- 11.  Crie uma consulta para calcular a quantidade total de carros em estoque.
 	
-   select sum(quantidade_estoque) from estoque;
+	select sum(quantidade_estoque) from estoque;
 
 -- 12.  Liste as compras feitas por um cliente específico, incluindo a data da compra e o total.
 
 -- 13.  Mostre os carros que nunca foram comprados.
 
 -- 14.  Mostre os clientes que ainda não fizeram nenhuma compra.
+
 
 
 
